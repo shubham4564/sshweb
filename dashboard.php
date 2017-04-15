21ffa1069f028a7fe9d83c99f8ca53b00930d630
shubham4564 <ycleptshubham@gmail.com>
2017-04-15 16:50:43 +0545

    Added comments

--- dashboard.php
+++ dashboard.php
@@ -10,13 +10,13 @@
 		   //take the password and username from the form value with POST method for little secure. 
 		   if(ssh2_auth_password($ssh, $uname, $passwd)) {
 		   		echo "You are logged in as : ";
-		        $stream = ssh2_exec($ssh, 'whoami');  //executes the command
-		        stream_set_blocking($stream, true);
-		        $data = '';
-		        while($buffer = fread($stream, 4096)) {
-		            $data .= $buffer;
+		        $stream = ssh2_exec($ssh, 'whoami');  //executes the command and keep it in Stream variable as resources
+		        stream_set_blocking($stream, true);  //blocking mode is turned to true for Stream variable
+		        $data = '';  //initialize data variable
+		        while($buffer = fread($stream, 4096)) {   //used loop to read from Stream variable upto length of 4096
+		            $data .= $buffer; // keep the value of buffer in data in every loop until the loop is complete
 		        }
-		        fclose($stream);
+		        fclose($stream); //close the read operation
 		        echo $data; // prints the data
 		    }
 }
